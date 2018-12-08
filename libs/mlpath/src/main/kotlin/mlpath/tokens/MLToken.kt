a2cb029be97d8d2914b7593f6c83cfd88e866c5a
Denis Korchuganov <DKorchuganov@yandex.ru>
2018-12-09 00:50:31 +0400

    Modified MLToken class in mlpath library

--- libs/mlpath/src/main/kotlin/mlpath/tokens/MLToken.kt
+++ libs/mlpath/src/main/kotlin/mlpath/tokens/MLToken.kt
@@ -3,9 +3,11 @@ package com.jvmlab.mlpath.tokens
 import com.jvmlab.commons.parse.text.Token
 
 
+/**
+ * Implementation of a generic [Token] class with [Types] parameter
+ */
 class MLToken(
     source: CharSequence,
     start: Int,
     stop: Int,
-    type: Types): Token<Types>(source, start, stop, type) {
-}
+    type: Types): Token<Types>(source, start, stop, type)
