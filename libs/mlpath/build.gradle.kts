bd71869b243030cb44b39ccf1d47c8c68b9e10ce
Denis Korchuganov <DKorchuganov@yandex.ru>
2018-12-09 07:16:21 +0400

    Added build scripts to repo

---
+++ libs/mlpath/build.gradle.kts
@@ -0,0 +1,18 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.10"
+}
+
+group = "com.jvmlab.libs"
+
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("com.jvmlab.libs:jvmlab-commons")
+}
+
