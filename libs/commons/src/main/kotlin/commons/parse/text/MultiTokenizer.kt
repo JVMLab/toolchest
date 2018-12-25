989e266bf97cc41a91c57a15193c9cc3bfc032fa
Denis Korchuganov <DKorchuganov@yandex.ru>
2018-12-26 01:02:15 +0400

    Started implementation of MultiTokenizer as part of #1
    1. Sub-tokenizers are stored in a List
    2. When no token is started (getBuildingStatus() method should return BuildingStatus.NONE) each incoming Char is processed by all sub-tokenizers, all tokenizers which have started a token are collected in a LinkedList: activeTokenizers
    
    Plus small refactoring of AbstractTokenizer to meet MultiTokenizer requirements

--- libs/commons/src/main/kotlin/commons/parse/text/MultiTokenizer.kt
+++ libs/commons/src/main/kotlin/commons/parse/text/MultiTokenizer.kt
@@ -1,20 +1,49 @@
 package com.jvmlab.commons.parse.text
 
+import java.util.*
+
 
 /**
  * Represents an [AbstractTokenizer] which produces tokens of multiple types by delegating
  * the actual work to sub-tokenizers
  *
- * @property subTokenizers is a [Map] of sub-tokenizers to produce actual tokens
+ * @property subTokenizers is a [List] of sub-tokenizers to produce actual tokens
+ * @property activeTokenizers is a [LinkedList] of sub-tokenizers with [BuildingStatus]
+ * different from [BuildingStatus.NONE] or [BuildingStatus.CANCELLED]
  */
 open class MultiTokenizer<E: Enum<E>>(
-    private val subTokenizers: Map<E, AbstractTokenizer<E>>
+    private val subTokenizers: List<AbstractTokenizer<E>>
 ) : AbstractTokenizer<E>() {
 
-  override fun firstChar(char: Char, idx: Int, isLast: Boolean) {
-    subTokenizers.forEach { type: E, tokenizer: AbstractTokenizer<E> ->
+  private val activeTokenizers: LinkedList<AbstractTokenizer<E>> = LinkedList()
 
+
+  /**
+   * Resets [activeTokenizers] and all [subTokenizers]
+   */
+  override fun reset() {
+    super.reset()
+    activeTokenizers.clear()
+    subTokenizers.forEach { it.reset() }
+  }
+
+
+  override fun firstChar(char: Char, idx: Int, isLast: Boolean) {
+    var isCancelled: Boolean = false
+
+    subTokenizers.forEach { tokenizer: AbstractTokenizer<E> ->
+      tokenizer.processChar(char, idx, isLast)
+      when (tokenizer.getBuildingStatus()) {
+        BuildingStatus.FAILED -> {
+          tokenBuilder = tokenizer.tokenBuilder
+          return
+        }
+        BuildingStatus.BUILDING, BuildingStatus.FINISHED -> activeTokenizers.add(tokenizer)
+        BuildingStatus.CANCELLED -> { isCancelled = true }
+        BuildingStatus.NONE -> {}
+      }
     }
+
     TODO()
   }
 
