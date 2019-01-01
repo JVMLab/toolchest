5ec2848728659ca021afd23f63683cb0ebd8e094
Denis Korchuganov <DKorchuganov@yandex.ru>
2019-01-01 21:12:26 +0400

    Implemented first*SubTokenizer() methods of BracketsTokenizer as part of #16

--- libs/commons/src/main/kotlin/commons/parse/text/BracketsTokenizer.kt
+++ libs/commons/src/main/kotlin/commons/parse/text/BracketsTokenizer.kt
@@ -6,22 +6,36 @@ package com.jvmlab.commons.parse.text
  *
  * @property leftBracket represents a left bracket [Char]
  * @property rightBracket represents a right bracket [Char]
+ * @property whiteSpaceAllowed indicates if whitespaces which are not part of a sub-token
+ * should be ignored (true) or reported as a token failure (false)
+ * @property maxNumberOfSubTokens maximum allowed number of sub-tokens expected from [subTokenizer],
+ * any negative value is treated as unlimited
  */
 class BracketsTokenizer<E: Enum<E>>(
     type: E,
     subTokenizer: AbstractTokenizer<E>,
     private val leftBracket: Char,
-    private val rightBracket: Char
+    private val rightBracket: Char,
+    private val whiteSpaceAllowed: Boolean = true,
+    private val maxNumberOfSubTokens: Int = -1
 ) : AbstractComplexTokenizer<E>(type, subTokenizer) {
 
 
   override fun firstBeforeSubTokenizer(char: Char, idx: Int, isLast: Boolean): BuildingDetails {
-    TODO("not implemented") // File | Settings | Editor | File and Code Templates.
+    processInSubTokenizer = false
+    if (char == leftBracket)
+      return (
+          if (isLast) BuildingDetails(BuildingStatus.FAILED,
+              "Unexpected $leftBracket as the last char at position $idx")
+          else BuildingDetails(BuildingStatus.BUILDING)
+          )
+
+    return BuildingDetails()
   }
 
 
   override fun firstAfterSubTokenizer(char: Char, idx: Int, isLast: Boolean): BuildingDetails {
-    TODO("not implemented") // File | Settings | Editor | File and Code Templates.
+    error("Unexpected call of firstAfterSubTokenizer() method")
   }
 
 
