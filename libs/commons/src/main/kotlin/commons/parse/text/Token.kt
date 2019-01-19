533d8e6e570bacf80532771f4269f1ec828ab261
Denis Korchuganov <DKorchuganov@yandex.ru>
2019-01-19 23:21:59 +0400

    Added AssertJ to build.gradle.kts and refactored tests of SingleCharTokenizer, closes #23
    Token class no longer needs equals() and hashcode() methods as we are using AssertJ assertions

--- libs/commons/src/main/kotlin/commons/parse/text/Token.kt
+++ libs/commons/src/main/kotlin/commons/parse/text/Token.kt
@@ -23,47 +23,25 @@ open class Token<E: Enum<E>>(open val type: E, startParam: Int, finishParam: Int
   open val finish: Int = finishParam
 
 
-    /**
-     * Gives [CharSequence] representation of this token based on a [source] [CharSequence]
-     */
-    open fun subSequence(source: CharSequence): CharSequence = source.subSequence(start, finish + 1)
+  /**
+   * Gives [CharSequence] representation of this token based on a [source] [CharSequence]
+   */
+  open fun subSequence(source: CharSequence): CharSequence = source.subSequence(start, finish + 1)
 
 
-    /**
-     * Pretty printing of this token based on a [source] [CharSequence]
-     *
-     * @param source is a source [CharSequence] for this token
-     */
-    open fun prettyPrint(source: CharSequence) =
-        println("$type : " + " ".repeat(start) + "'${subSequence(source).toString()}'")
+  /**
+   * Pretty printing of this token based on a [source] [CharSequence]
+   *
+   * @param source is a source [CharSequence] for this token
+   */
+  open fun prettyPrint(source: CharSequence) =
+      println("$type : " + " ".repeat(start) + "'${subSequence(source).toString()}'")
 
 
-    /**
-     * Gives length of the token in characters
-     */
-    open fun length(): Int = finish - start + 1
-
-
-  override fun equals(other: Any?): Boolean {
-    if (this === other) return true
-    if (javaClass != other?.javaClass) return false
-
-    other as Token<*>
-
-    if (type != other.type) return false
-    if (start != other.start) return false
-    if (finish != other.finish) return false
-
-    return true
-  }
-
-
-  override fun hashCode(): Int {
-    var result = type.hashCode()
-    result = 31 * result + start
-    result = 31 * result + finish
-    return result
-  }
+  /**
+   * Gives length of the token in characters
+   */
+  open fun length(): Int = finish - start + 1
 
 
   override fun toString(): String {
