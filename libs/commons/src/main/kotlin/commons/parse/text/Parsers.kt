7a1deb8632b9650e423e7fa8f952ea819969d01c
Denis Korchuganov <dkorchuganov@yandex.ru>
2020-05-02 14:24:11 +0300

    COMMONS-9: Refactor ITokenizer and its implementations
    as well as Token and TokenBuilder classes
    
    Added Parsed with tests

--- libs/commons/src/main/kotlin/commons/parse/text/Parsers.kt
+++ libs/commons/src/main/kotlin/commons/parse/text/Parsers.kt
@@ -15,14 +15,12 @@ import com.jvmlab.commons.parse.ParsedKey
  * @param tokenizer is an [ITokenizer] used to split [this] [CharSequence] into tokens
  * @param defaultTokenType is a type of a default token, which is created whenever the [tokenizer]
  */
-/*
 fun <E: Enum<E>> CharSequence.parse(
-    tokenizer: ITokenizer<E>,
+    tokenizer: IStartTokenizer<E>,
     defaultTokenType: E? = null): Parsed<CharSequence, List<Token<E>>> =
     Parsed(this) { charSequence -> parse(charSequence, tokenizer, defaultTokenType) }
 
 
-*/
 /**
  * Private function which does all heavy-lifting to parse a [charSequence], all public functions
  * are just wrappers around it.
@@ -35,83 +33,93 @@ fun <E: Enum<E>> CharSequence.parse(
  *
  * @return a single key [Map] with list of tokens produced by [tokenizer] along with default tokens
  * (in case of [defaultTokenType] is not null) as a value and [ParsedKey.PARSED_STRING.key] key
- *//*
-
+ *
+ */
 private fun <E: Enum<E>> parse(
     charSequence: CharSequence,
-    tokenizer: ITokenizer<E>,
+    tokenizer: IStartTokenizer<E>,
     defaultTokenType: E?
 ): Map<String, List<Token<E>>> {
   val tokenList = ArrayList<Token<E>>() // a list of tokens to be returned as a value in the Map
   var token: Token<E> // a token built by the currentBuilder
-  var defaultStart: Int = 0 // a start of a default token, used when defaultTokenType is not null
-  var idx: Int = 0 // current index in the charSequence
+  var defaultStart = 0 // a start of a default token, used when defaultTokenType is not null
+  var idx = 0 // current index in the charSequence
   var char: Char // current char in the charSequence
-  var isLast: Boolean // true if char is the last Char in the charSequence
+  var status: TokenizerStatus = StatusNone<E>(tokenizer)
 
-  while (idx < charSequence.length) {
+
+  while (idx < charSequence.length - 1) {
     char = charSequence[idx]
-    isLast = (idx == charSequence.length - 1)
-    when (val status = tokenizer.getBuildingStatus()) {
+    when (status) {
 
-      StatusNone -> {
-        tokenizer.processChar(char, idx, isLast)
-        if (isLast) {
-          if (StatusNone == tokenizer.getBuildingStatus()) {
-            idx++ // to exit the loop
-            // Add a default token if the last char doesn't start a token
-            tokenList.addDefaultToken(defaultTokenType, defaultStart, idx)
-          }
-          // don't increment idx here to process a new BuildingStatus in the next iteration
-        } else {
-          idx++
+      is StatusNone<*> -> {
+        status = status.startProcessing(char, idx)
+        when (status) {
+          is StatusNone<*>,
+          is StatusBuilding<*> -> idx++
+          is FinalModifiedStatus -> { /* just skip, will be processed in the next iteration */ }
         }
       }
 
-      StatusBuilding -> {
-        // changes this.status and may increment this.finish if still in building
-        tokenizer.processChar(char, idx, isLast)
-        if (isLast) {
-          // don't increment idx here to process a new status in the next iteration
-          if (StatusBuilding == tokenizer.getBuildingStatus())
-            throw IllegalStateException(
-                "Unexpected state: $StatusBuilding at the last position $idx")
-        } else {
-          // increments idx if still in building or
-          // keeps it when this.finish was not incremented in tokenizer.processChar()
-          idx = tokenizer.getCurrentFinish() + 1
+      is StatusBuilding<*> -> {
+        // increments idx if still in building or
+        // keeps it when this.finish was not incremented in tokenizer.processChar()
+        status = status.processChar(char)
+        when (status) {
+          is StatusBuilding<*> -> idx = status.finish + 1
+          is FinalModifiedStatus -> { /* just skip, will be processed in the next iteration */ }
+          is StatusNone<*> -> throw IllegalStateException("Unexpected $status at position $idx")
         }
       }
 
-      StatusFinished -> {
-        token = tokenizer.buildToken()
+      is StatusFinished<*> -> {
+        @Suppress("UNCHECKED_CAST")
+        token = status.createToken() as Token<E>
+        status = status.reset()
         tokenList.addDefaultToken(defaultTokenType, defaultStart, token.start)
         tokenList.add(token)
         idx = token.finish + 1 // sets idx to a next char after the current token
         defaultStart = idx
       }
 
-      StatusCancelled -> {
-        if (isLast) // Add a default token till the end of charSequence in case of the last char
-          tokenList.addDefaultToken(defaultTokenType, defaultStart, idx)
-        idx = tokenizer.getCurrentFinish() + 1
+      is StatusCancelled<*> -> {
+        idx = status.finish + 1
+        status = status.reset()
       }
 
-      is StatusFailed -> {
-        throw IllegalStateException("Parsing error at position $idx with reason: ${status.reason}")
-      }
+      is StatusFailed<*> -> throw IllegalStateException("Parsing error at position $idx with reason: ${status.reason}")
     }
+  }
+
+  //idx == charSequence.length - 1
+  char = charSequence[idx]  // it's not yet parsed
+
+  status = when (status) {
+    is StatusNone<*> -> status.startProcessingLast(char, idx)
+    is StatusBuilding<*> -> status.processLastChar(char)
+    is FinalModifiedStatus -> throw IllegalStateException("Unexpected $status before position $idx")
+  }
+
+  when (status) {
+    is StatusNone<*>,
+    is StatusCancelled<*> -> tokenList.addDefaultToken(defaultTokenType, defaultStart, idx + 1)
+    is StatusFinished<*> -> {
+      @Suppress("UNCHECKED_CAST")
+      token = status.createToken() as Token<E>
+      tokenList.addDefaultToken(defaultTokenType, defaultStart, token.start)
+      tokenList.add(token)
+    }
+    is StatusBuilding<*> -> throw IllegalStateException("Unexpected $status at position $idx")
+    is StatusFailed<*> -> throw IllegalStateException("Parsing error at position $idx with reason: ${status.reason}")
   }
 
   return mapOf(ParsedKey.PARSED_STRING.key to tokenList)
 }
 
 
-*/
 /**
  * Adds a default token if required
- *//*
-
+ */
 private fun <E: Enum<E>> MutableList<Token<E>>.addDefaultToken(
     defaultTokenType: E?,
     defaultStart: Int,
@@ -120,4 +128,4 @@ private fun <E: Enum<E>> MutableList<Token<E>>.addDefaultToken(
     if (defaultStart < nextTokenStart)
       this.add(Token<E>(it, defaultStart, nextTokenStart - 1))
   }
-}*/
+}
