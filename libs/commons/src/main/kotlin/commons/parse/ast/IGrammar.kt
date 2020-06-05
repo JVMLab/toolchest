87e6039897960fdc06271e4b546027e30561d42f
Denis Korchuganov <dkorchuganov@yandex.ru>
2020-06-05 23:13:44 +0300

    COMMONS-13: Implement Grammar
    Basic IGrammar implementation

--- libs/commons/src/main/kotlin/commons/parse/ast/IGrammar.kt
+++ libs/commons/src/main/kotlin/commons/parse/ast/IGrammar.kt
@@ -6,9 +6,10 @@ import com.jvmlab.commons.parse.lexer.Token
 
 
 /**
- * Interface to be used by a parser to get an AST node [INode] for a [Token]
+ * Interface to be used by a parser to get an AST node [INode] for a [Token]. If no node is found
+ * for a token then null is returned
  */
-interface IGrammar {
-  fun <E: Enum<E>> getOperand(token: Token<E>): INode<*>
-  fun <E: Enum<E>> getBinaryOperator(token: Token<E>): IBinaryOperator<*, *, *>
+interface IGrammar<E: Enum<E>> {
+  fun getOperand(token: Token<E>): INode<*>?
+  fun getBinaryOperator(token: Token<E>): IBinaryOperator<*, *, *>?
 }
