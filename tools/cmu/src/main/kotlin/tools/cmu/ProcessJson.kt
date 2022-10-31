cf29f720cf16aed3c57a9ba2109b902342467008
Denis Korchuganov <dkorchuganov@yandex.ru>
2022-10-31 13:13:19 +0300

    cmu: JSON pretty-printing and lenient parsing

--- tools/cmu/src/main/kotlin/tools/cmu/ProcessJson.kt
+++ tools/cmu/src/main/kotlin/tools/cmu/ProcessJson.kt
@@ -2,9 +2,18 @@ package com.jvmlab.tools.cmu
 
 import java.io.File
 import java.io.PrintWriter
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
 
+
+private val parser = Json {
+  prettyPrint = true
+  isLenient = true
+}
 
 fun processJson(inputFile: File, printWriter: PrintWriter) {
-  val inputString = inputFile.readText()
-  printWriter.print(inputString)
+  val jsonElement = parser.decodeFromString<JsonElement>(inputFile.readText())
+  printWriter.print(parser.encodeToString(jsonElement))
 }
